a4e3f85d0f5855d2d31aa8dcf1c65617b9e0bb97
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-07-19 11:00:16 +0500

    create project

---
+++ app/src/main/java/com/example/jk_0141_slots/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jk_0141_slots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
