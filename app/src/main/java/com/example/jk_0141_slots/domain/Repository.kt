5f3727dcd2cfd9c01b57306b876c9cc0cfd6d056
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-07-22 18:19:45 +0500

    add room local bd

--- app/src/main/java/com/example/jk_0141_slots/domain/Repository.kt
+++ app/src/main/java/com/example/jk_0141_slots/domain/Repository.kt
@@ -1,10 +1,16 @@
 package com.example.jk_0141_slots.domain
 
 import com.example.jk_0141_slots.data.local.HistoryDao
+import com.example.jk_0141_slots.data.local.models.HistoryModel
 import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val daoHistory: HistoryDao
 ){
-
+    suspend fun getHistory(): List<HistoryModel>{
+        return daoHistory.getHistory()
+    }
+    suspend fun addHistory(data: HistoryModel){
+        daoHistory.insertHistory(data)
+    }
 }
